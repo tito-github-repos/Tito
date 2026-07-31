e00569a26bf2f40f35c9dac95bc6dfd7b7575f4c
Manishasubburajs <manisha@tito.org.in>
2026-07-31 12:01:10 +0530

    home page hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import OurTeamSection from "./components/home/ourteam";
 import OurServices from "./components/home/ourservices";
 import OurExpertise from "./components/home/ourexpertise";
+import Homehero from "./components/home/homehero";
 
 export default function Home() {
   return (
     <>
+      <Homehero />
       <OurTeamSection />
       <OurServices />
       <OurExpertise />
